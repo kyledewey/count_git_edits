82063e8e20f96dec29b80f81d5d2a2a9bd36946f
Kyle Dewey <kyle.dewey@csun.edu>
2019-01-08 12:29:26 -0800

    hackery; cannot get jgit working from console

--- build.sbt
+++ build.sbt
@@ -7,3 +7,4 @@ scalaVersion := "2.12.4"
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
 
 libraryDependencies += "org.eclipse.jgit" % "org.eclipse.jgit" % "5.2.0.201812061821-r"
+libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.25"
---
+++ src/main/scala/count_git_edits/counter.scala
@@ -0,0 +1,23 @@
+package count_git_edits
+
+/*
+import java.io.File
+
+object Counter {
+  val DEFAULT_GIT_DIR = "."
+
+  def apply(): Counter = {
+    apply(new File(DEFAULT_GIT_DIR))
+  }
+
+  def apply(gitDirectory: File): Counter = {
+    import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+    new FileRepositoryBuilder()
+      .setGitDir(gitDirectory)
+      .build()
+  }
+}
+
+class Counter(private val git: Git) {
+  def counts(
+ */
